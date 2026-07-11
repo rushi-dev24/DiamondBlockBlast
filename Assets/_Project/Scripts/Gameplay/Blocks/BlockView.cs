74ef385caa7ce7e613c7c94ee803faf3084e0e08
rushi-dev24 <rushi-dev24@outlook.com>
2026-07-11 12:32:57 +0530

    feat(input): implement drag and drop foundation for block views

--- Assets/_Project/Scripts/Gameplay/Blocks/BlockView.cs
+++ Assets/_Project/Scripts/Gameplay/Blocks/BlockView.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace BlockPuzzle.Gameplay.Blocks
 {
-    public sealed class BlockView : MonoBehaviour
+    public sealed class BlockView :
+        MonoBehaviour,
+        IBeginDragHandler,
+        IDragHandler,
+        IEndDragHandler
     {
         [SerializeField]
         private BlockVisualCell visualCellPrefab;
@@ -14,10 +19,25 @@ namespace BlockPuzzle.Gameplay.Blocks
         private float cellSize = 50f;
 
         private RectTransform rectTransform;
+        private Canvas canvas;
+        private CanvasGroup canvasGroup;
+
+        private Vector2 originalPosition;
 
         private void Awake()
         {
             rectTransform = GetComponent<RectTransform>();
+
+            canvas = GetComponentInParent<Canvas>();
+
+            canvasGroup =
+                GetComponent<CanvasGroup>();
+
+            if (canvasGroup == null)
+            {
+                canvasGroup =
+                    gameObject.AddComponent<CanvasGroup>();
+            }
         }
 
         public void Initialize(BlockData blockData)
@@ -59,11 +79,37 @@ namespace BlockPuzzle.Gameplay.Blocks
                     (maxY + 1) * cellSize);
         }
 
+        public void OnBeginDrag(
+            PointerEventData eventData)
+        {
+            originalPosition =
+                rectTransform.anchoredPosition;
+
+            canvasGroup.blocksRaycasts = false;
+        }
+
+        public void OnDrag(
+            PointerEventData eventData)
+        {
+            rectTransform.anchoredPosition +=
+                eventData.delta / canvas.scaleFactor;
+        }
+
+        public void OnEndDrag(
+            PointerEventData eventData)
+        {
+            canvasGroup.blocksRaycasts = true;
+
+            rectTransform.anchoredPosition =
+                originalPosition;
+        }
+
         private void Clear()
         {
             for (int i = cellParent.childCount - 1; i >= 0; i--)
             {
-                Destroy(cellParent.GetChild(i).gameObject);
+                Destroy(
+                    cellParent.GetChild(i).gameObject);
             }
         }
     }
